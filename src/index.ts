24e746a7a40a231c06e1391d09cc50fbeabb321c
Luke Kennedy <luke.l.kennedy@gmail.com>
2021-02-23 22:41:38 +1100

    Add type guards

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 /** Sleeps for the requested number of milliseconds, blocking return until the timeout is complete. Await this function to sleep. */
 export async function sleep(millis: number) {
+	if (millis === undefined || typeof millis !== "number" || isNaN(millis)) {
+		throw new Error(NaN.toString());
+	}
 	await new Promise(res => {
 		setTimeout(res, millis);
 	})
